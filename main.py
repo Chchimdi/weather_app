66094d95ea7a1a3d8a0a09ed67618b8a43737a01
Chimdi Chikezie <chikeziechimdi@gmail.com>
2024-02-21 22:14:48 -0800

    Deleted ui.py from test_main branch

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from testfile import weather_zipcode, weather_city 
 
 
-def fetch_weather(user_input):
+def fetch_weather():
     with open('appid.txt', 'r') as file:
         appid = file.readline().strip()
+
+    user_input = input("Enter city or zipcode: ")
 
     try:
         if user_input.isdigit():
             return weather_zipcode(user_input, appid)
         else:
-            return weather_zipcode(user_input, appid)
+            return weather_city(user_input, appid)
     except KeyError:
         print("Not found. Please enter a valid zipcode or city name.")
     except ValueError:
         print("Invalid input. Please try again.")
 
 print("Welcome to Chimdi's Weather App")
-user_input = input("Enter city or zipcode: ")
-print(fetch_weather(user_input))
+
+print(fetch_weather())
